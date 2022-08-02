4c2f3ad1ad569f2f60caa5a0193cc9d50fa1a3c0
adamsburge <adam.scott.burge@gmail.com>
2022-08-02 10:51:05 +0000

    Add basic outline of JavaScript functions

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// Array to store the questions for the quiz
+
+
+
+// Global Variables
+
+
+
+// Functions
+
+function startQuiz() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function nextQuestion() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementProgressBar {
+    
+}
