6059b43e5e7dfd851b69ca8eecb0a7f97463297b
adamsburge <adam.scott.burge@gmail.com>
2022-08-03 22:31:52 +0000

    Add Americas questions to the question bank in JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,58 @@
 // Array to store the questions for the quiz
+const quizQuestionData = [
+    {
+        continent: "americas",
+        initialAnswerPhrase: "Colombia is the country that borders Panama, Venezuela, Brazil, Peru and Ecuador.",
+        funFact: "Colombia is also the only country in South America to have beaches in both the Pacific Ocean and the caribbean Sea.",
+        hint:  {
+            hintText: "Take a look at this map which labels the border countries. See if you know the country in question by its location",
+            hintImg: ""
+        },
+        quiz:  {
+            question: "Which country has Panama, Venezuela, Brazil, Peru and Ecuador as its bordering neighbors?",
+            a: "Argentina",
+            b: "Colombia",
+            c: "Bolivia",
+            d: "Chile",
+            correct: "b"
+        }
+    },
+    {
+        continent: "americas",
+        initialAnswerPhrase: "The Missouri River is the longest river in North America.",
+        funFact: "The Missouri river flows into North America’s second largest river, the Mississippi river. When combined, these two make up the world’s fourth largest river system",
+        hint: {
+            hintText: "Take a look at this Map and compare the rivers you see!",
+            hintImg: ""
+        },
+        quiz: {
+            question: "What is the longest river in North America?",
+            a: "Missouri River",
+            b: "Mississippi River",
+            c: "Colorado River",
+            d: "Yukon River",
+            correct: "a"
+        }
 
+    },
+    {
+        continent: "americas",
+        initialAnswerPhrase: "The highest mountain in the americas is Aconcagua in Mendoza, Argentina.",
+        funFact: "Not only is Aconcagua the highest mountain in the Americas, it is also the highest mountain outside Asia. Its summit is at an elevation of 6,961 meters (22,838 ft)",
+        hint: {
+            hintText: "Look around this map and see if you can discern where the highest mountain might be.",
+            hintImg: ""
+        },
+        quiz: {
+            question: "Where is the highest mountain in the Americas?",
+            a: "Colorado, USA",
+            b: "Mendoza, Argentina",
+            c: "Alaska, USA",
+            d: "Yungay, Peru",
+            correct: "b"
+        }
+    }
+]
 
 
 // Global Variables
